190bf2dd632508c27ed0de9af229be05a1204cdc
Bharath <reddybharath932@gmail.com>
2025-11-21 21:24:58 +0530

    refactor: remove commented-out Gmail API example code and add comments for pipeline execution.

--- main.go
+++ main.go
@@ -5,22 +5,8 @@ import (
 )
 
 func main() {
+	//Loading Env
 	godotenv.Load()
-	
-	run_pipeline()
-	// fmt.Println(os.Getenv("GOOGLE_API_KEY"))
-	// agent_init()
-	
-	// r, err := srv.Users.Labels.List("me").Do()
-	// if err != nil {
-	// 	log.Fatalf("Unable to retrieve labels: %v", err)
-	// }
-	// if len(r.Labels) == 0 {
-	// 	fmt.Println("No labels found.")
-	// 	return
-	// }
-	// fmt.Println("Labels:")
-	// for _, l := range r.Labels {
-	// 	fmt.Printf("- %s\n", l.Name)
-	// }
+	//Pipeline Run
+	run_pipeline()	
 }
